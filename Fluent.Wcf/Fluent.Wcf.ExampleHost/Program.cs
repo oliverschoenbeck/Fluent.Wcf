c2138da57ffc600dd247a72e27708929bb4c9853
Oliver <os@oliver-schoenbeck.de>
2016-06-07 20:37:54 +0200

    First checkin

---
+++ Fluent.Wcf/Fluent.Wcf.ExampleHost/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluent.Wcf.ExampleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
